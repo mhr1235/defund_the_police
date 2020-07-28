e78a588bd39313dffbfe6363ed19472ab7108bd5
mhr1235 <65504141+mhr1235@users.noreply.github.com>
2020-07-28 15:11:23 -0400

    Update image_array.js
    got rid of individual push images. using a loop.

--- image_array.js
+++ image_array.js
@@ -1,28 +1,11 @@
-// don't use new Array(3), just use []
-var i, images = [];
-// use push, don't manually assign the index
-images.push("images/image_1.jpg");
-images.push("images/image_2.jpg");
-images.push("images/image_3.jpg");
-images.push("images/image_4.jpg");
-images.push("images/image_5.jpg");
-images.push("images/image_6.jpg");
-images.push("images/image_7.jpg");
-images.push("images/image_8.jpg");
-images.push("images/image_9.jpg");
-images.push("images/image_10.jpg");
-images.push("images/image_11.jpg");
-images.push("images/image_12.jpg");
-images.push("images/image_13.jpg");
-images.push("images/image_14.jpg");
-images.push("images/image_15.jpg");
-images.push("images/image_16.jpg");
-images.push("images/image_17.jpg");
-images.push("images/image_18.jpg");
-images.push("images/image_19.jpg");
-images.push("images/image_20.jpg");
-images.push("images/image_21.jpg");
-//just add more images to creeate a larger pool
+var numImages = 21;
+
+var images = [];
+
+for(let a=1; a<=numImages;a++){
+    images.push("images/image_"+a+".jpg");
+    console.log(a);
+}
 
 images.sort(function ()
 {
