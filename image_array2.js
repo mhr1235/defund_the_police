47a333c8f38b0c7e0e6081ed04d140168d5f6f59
mhr1235 <65504141+mhr1235@users.noreply.github.com>
2020-08-02 20:27:38 -0400

    Create image_array2.js

---
+++ image_array2.js
@@ -0,0 +1,99 @@
+// don't use new Array(3), just use []
+var numImages = 21;
+
+var images = [];
+
+var click_counter = 0;
+
+var random_test;
+
+
+
+
+
+//var i, images = [];
+// use push, don't manually assign the index
+/*images.push("images/image_2.jpg");
+images.push("images/image_3.jpg");
+images.push("images/image_4.jpg");
+images.push("images/image_5.jpg");
+images.push("images/image_6.jpg");
+images.push("images/image_7.jpg");
+images.push("images/image_8.jpg");
+images.push("images/image_9.jpg");
+images.push("images/image_10.jpg");
+images.push("images/image_11.jpg");
+images.push("images/image_12.jpg");
+images.push("images/image_13.jpg");
+images.push("images/image_14.jpg");
+images.push("images/image_15.jpg");
+images.push("images/image_16.jpg");
+images.push("images/image_17.jpg");
+images.push("images/image_18.jpg");
+images.push("images/image_19.jpg");
+images.push("images/image_20.jpg");
+images.push("images/image_21.jpg");*/
+
+//just add more images to creeate a larger pool
+
+
+for(let a=1; a<=numImages;a++){
+    images.push("images/image_"+a+".jpg");
+    //console.log(a);
+
+}
+
+
+images.sort(function ()
+{
+    return 0.5 - Math.random();
+});
+
+
+
+for (var i = 0; i < 9; i++){
+
+document.getElementsByClassName('captcha-image')[i].innerHTML = "<img src='"+images[i]+"'>";
+document.getElementsByClassName('captcha-image')[i].addEventListener("click",clicks);
+
+
+}
+
+
+
+ 
+
+
+function clicks(){
+
+
+	if(!$(this).hasClass("active")){
+      //alert("button was clicked");
+      click_counter++;
+//console.log(click_counter);
+
+	}
+	if($(this).hasClass("active")){
+      //alert("button was clicked");
+      click_counter--;
+//console.log(click_counter);
+
+	}
+}
+
+   document.getElementById('verify').onclick = function() {
+
+random_test = Math.floor((Math.random() * 8)+ 1);
+console.log(random_test);
+
+if(click_counter == random_test){
+
+//window.location.href = 'https://www1.nyc.gov/site/nypd/about/about-nypd/about-nypd-landing.page';
+window.location.href = 'kkkops_p2.html';
+
+
+}else{
+location.reload();
+window.location.href = 'kkkops_p3.html';
+}
+};
